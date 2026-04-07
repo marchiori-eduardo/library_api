878bfd43be81d674445c804e73ceff0dd936579e
marchiori-eduardo <marchiorimarchiori2@gmail.com>
2026-04-07 20:14:11 -0300

    Initial commit (project creation)

---
+++ src/test/java/marchiori_eduardo/com/github/libraryapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package marchiori_eduardo.com.github.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
